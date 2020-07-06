58bf3c7ce552e015c3049f7e316ba70853d5de87
Yusuke Yamane <yusukeyamane01@gmail.com>
2020-07-07 08:42:10 +0900

    fix

--- src/components/rps/hand/index.tsx
+++ src/components/rps/hand/index.tsx
@@ -1,111 +1,36 @@
 import React from "react";
-import rock from "../../../images/rock.jpeg";
-import scissors from "../../../images/scissors.jpeg";
-import paper from "../../../images/paper.jpeg";
 import { RpsType } from "../rpsType";
 
 interface Props {
   clickHand?: (val: RpsType) => void;
   selectHand?: RpsType;
-  enemyHand?: RpsType;
+  handType: RpsType;
+  selected: Boolean;
 }
 
 export default class Index extends React.Component<Props> {
-  handTypes: RpsType[] = [RpsType.rock, RpsType.scissors, RpsType.paper];
-
-  getOpacity(hand: RpsType, handType?: RpsType) {
-    if (this.props.enemyHand !== undefined) {
-      return 1;
-    }
-    switch (hand) {
-      case RpsType.rock:
-        if (handType !== RpsType.rock) {
-          return 0.3;
-        }
-        break;
-      case RpsType.scissors:
-        if (handType !== RpsType.scissors) {
-          return 0.3;
-        }
-        break;
-
-      case RpsType.paper:
-        if (handType !== RpsType.paper) {
-          return 0.3;
-        }
-        break;
-    }
-  }
-
-  getHand(handType: RpsType) {
+  render() {
     const imgSize = 100;
+    const handType = RpsType[this.props.handType];
 
-    switch (handType) {
-      case RpsType.rock:
-        return (
+    return (
+      <div style={{ display: "flex" }}>
+        <button
+          onClick={() => {
+            this.setState({ opacity: 1.0 })
+            this.props.clickHand && this.props.clickHand(this.props.handType);
+          }}
+        >
           <img
-            src={rock}
+            src={`images/${handType}.jpeg`}
             alt="rock"
             style={{
               height: imgSize,
               width: imgSize,
-              opacity: this.getOpacity(RpsType.rock, this.props.selectHand),
+              opacity: this.props.selected ? '1.0' : '0.3',
             }}
           />
-        );
-      case RpsType.scissors:
-        return (
-          <img
-            src={scissors}
-            alt="scissors"
-            style={{
-              height: imgSize,
-              width: imgSize,
-              opacity: this.getOpacity(RpsType.scissors, this.props.selectHand),
-            }}
-          />
-        );
-      case RpsType.paper:
-        return (
-          <img
-            src={paper}
-            alt="paper"
-            style={{
-              height: imgSize,
-              width: imgSize,
-              opacity: this.getOpacity(RpsType.paper, this.props.selectHand),
-            }}
-          />
-        );
-    }
-  }
-
-  renderHand() {
-    const listItems = this.handTypes.map((handType: RpsType) => (
-      <button
-        onClick={() => {
-          this.props.clickHand && this.props.clickHand(handType);
-        }}
-        key={handType}
-      >
-        {this.getHand(handType)}
-      </button>
-    ));
-    return listItems;
-  }
-
-  renderEnemyHand() {
-    if (this.props.enemyHand !== undefined) {
-      return <div>{this.getHand(this.props.enemyHand)}</div>;
-    }
-    return null;
-  }
-
-  render() {
-    return (
-      <div style={{ display: "flex" }}>
-        {this.props.enemyHand === undefined && this.renderHand()}
-        {this.props.enemyHand !== undefined && this.renderEnemyHand()}
+        </button>
       </div>
     );
   }
--- src/components/rps/index.tsx
+++ src/components/rps/index.tsx
@@ -13,6 +13,8 @@ interface State {
 }
 
 export default class Index extends React.Component<Props, State> {
+  handTypes: RpsType[] = [RpsType.rock, RpsType.scissors, RpsType.paper];
+
   constructor(props: Props) {
     super(props);
     this.state = {
@@ -31,11 +33,11 @@ export default class Index extends React.Component<Props, State> {
   componentDidUpdate(prevProps: Props, prevState: State) {
     if (
       this.state.selectHand !== undefined &&
-      prevState.selectHand !== this.state.selectHand
+      (prevState.battleCount !== this.state.battleCount)
     ) {
       const enemyHand: RpsType = Math.floor(Math.random() * 3);
       this.setState({ enemyHand: enemyHand });
-      this.resultbattle(this.state.selectHand, enemyHand);
+      this.confirmResult(this.state.selectHand, enemyHand);
     }
   }
 
@@ -46,11 +48,11 @@ export default class Index extends React.Component<Props, State> {
   }
 
   handleOnClick(val: RpsType) {
+    this.setState({ battleCount: this.state.battleCount + 1 });
     this.setState({ selectHand: val });
   }
 
-  resultbattle(selectHand: RpsType, enemyHand: RpsType) {
-    this.setState({ battleCount: this.state.battleCount + 1 });
+  confirmResult(selectHand: RpsType, enemyHand: RpsType) {
     if (selectHand === enemyHand) {
       return this.setState({ resultString: "あいこで" });
     }
@@ -99,13 +101,29 @@ export default class Index extends React.Component<Props, State> {
       >
         <h1>じゃんけんページ</h1>
         <h2>自分の手</h2>
-        <Hand
-          selectHand={this.state.selectHand}
-          clickHand={(val: RpsType) => this.handleOnClick(val)}
-        />
+        <div style={{ display: "flex" }}>
+          {this.handTypes.map((handType: RpsType) => (
+            <Hand
+              selectHand={this.state.selectHand}
+              clickHand={(val: RpsType) => this.handleOnClick(val)}
+              handType={handType}
+              selected={this.state.selectHand === handType}
+              key={handType}
+            />
+          ))}
+        </div>
         <h1 style={{ color: "red" }}>{this.state.resultString}</h1>
         <h2 style={{ marginTop: 10 }}>相手の手</h2>
-        <Hand enemyHand={this.state.enemyHand} />
+        <div style={{ display: "flex" }}>
+          {this.handTypes.map((handType: RpsType) => (
+            <Hand
+              selectHand={this.state.enemyHand}
+              handType={handType}
+              selected={this.state.enemyHand === handType}
+              key={handType}
+            />
+          ))}
+        </div>
         <Link to="/">
           <h1>じゃんけんを終了する</h1>
           <h4>※戦績が発表されます。</h4>
